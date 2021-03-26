9b42ba4191d622a9aba5ebbadff5f2f752d12387
meowz212 <57517480+meowz212@users.noreply.github.com>
2021-03-26 14:41:11 +0700

    Fixed socket and 16-last characters checking

--- progjar1/clientmod1.py
+++ progjar1/clientmod1.py
@@ -3,33 +3,35 @@ import socket
 import string
 import random
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-i = 2
-servers = ["192.168.122.156", "192.168.122.105"]
+serv_port = [5000, 5002]
 
-# Connect the socket to the port where the server is listening
+
 for i in range(2):
-    server_address = (servers[i], 10000)
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect the socket to the port where the server is listening
+    server_address = ('192.168.164.128', serv_port[i])
     print(f"connecting to {server_address}")
     sock.connect(server_address)
 
-try:
-    loopnum = 2097152
-    # Send data
-    message = ''.join(random.choice(string.ascii_lowercase) for x in range(loopnum))
+    try:
+        loopnum = 2097152
+        # Send data
+        message = ''.join(random.choice(string.ascii_lowercase) for x in range(loopnum))
 
-    print(f"output size: ", len(message) / 1024, "kb")
-    print(f"sending string")
-    sock.sendall(message.encode())
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print(f"{data}")
-finally:
-    print("closing")
-    sock.close()
+        print(f"output size: ", len(message) / 1024, "kb")
+        print(f"sending string")
+        sock.sendall(message.encode())
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(message)
+        while amount_received < (amount_expected + 12) :
+            data = sock.recv(16)
+            amount_received += len(data)
+
+    finally:
+        print("closing")
+        print("last 16 strings: " + message[-16:])
+        sock.close()
