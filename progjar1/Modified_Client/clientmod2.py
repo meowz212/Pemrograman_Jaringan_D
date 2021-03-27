885ab8c924f6fa970e38375718f6c111ceb52460
meowz212 <57517480+meowz212@users.noreply.github.com>
2021-03-27 10:40:59 +0700

    Add Decryption Post - Receive

--- progjar1/Modified_Client/clientmod2.py
+++ progjar1/Modified_Client/clientmod2.py
@@ -7,20 +7,21 @@ import base64
 
 serv_port = [5000, 5002]
 
+name = input("Please input your expected filename:")
 
 for i in range(2):
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Connect the socket to the port where the server is listening
-    server_address = ('192.168.164.128', 5000)
+    server_address = ('192.168.164.128', serv_port[i])
     print(f"connecting to {server_address}")
     sock.connect(server_address)
 
     try:
         image = 'tes.png'
         message = os.path.getsize(image)
-        name = input("Please input your expected filename:")
+
 
         # Send data
         print(f"sending {message} Bytes of Data")
@@ -31,18 +32,29 @@ for i in range(2):
 
         # Look for the response
         amount_received = 0
-        f2 = open(name + "_" + str(i) + ".png", 'wb')
+        f2 = open('encrypted' + str(i) + '.png', 'wb')
         amount_expected = len(l)
-        while amount_received < amount_expected:
+        while amount_received < (amount_expected + 12):
 
             data = sock.recv(16)
-            encode = base64.b64decode(data)
-            f2.write(encode)
+            f2.write(data)
             amount_received += len(data)
 
         # print(f"{amount_received}")
         f2.close()
 
+        #Open the encrypted file
+        with open('encrypted' + str(i) + '.png', 'rb') as f:
+            l = base64.b64decode(f.read())
+
+        #Encode the encryted Base64 Image
+        with open(name + "_" + str(i) + ".png", 'wb') as f2:
+            f2.write(l)
+            f2.close()
+
+        f.close()
+
     finally:
         print("closing")
+        os.remove('encrypted' + str(i) + '.png')
         sock.close()
